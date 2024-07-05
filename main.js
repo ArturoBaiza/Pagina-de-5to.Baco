c690d5d8c7cadf3dba684635f8e387e12613b3a1
ArthurBaiza01 <138905503+ArturoBaiza@users.noreply.github.com>
2024-07-04 19:56:00 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+// Script para abrir Gmail en la misma pestaña al hacer clic en el botón
+document.getElementById('btn-contacto').addEventListener('click', function() {
+  window.location.href = 'https://mail.google.com/mail/?view=cm&fs=1&to=feriacientifica5tobaco@gmail.com';
+});
+
+document.getElementById('btn-probar').addEventListener('click', function() {
+  window.location.href = 'https://juegos-5to-baco.onrender.com/';
+});
+
+document.getElementById('btn-saber').addEventListener('click', function() {
+  window.location.href = 'https://estudiantes-5to-baco.onrender.com';
+});
+
+document.getElementById('btn-ver').addEventListener('click', function() {
+  window.location.href = 'https://informacion-de-juego-fisico.onrender.com';
+});
+
+document.getElementById('btn-con').addEventListener('click', function() {
+  window.location.href = 'https://archivo-de-carrusel.onrender.com';
+});
+
+document.getElementById('btn-conz').addEventListener('click', function() {
+  window.location.href = 'https://carrusel-de-imagenes.onrender.com';
+});
+
+document.getElementById('btn-conoz').addEventListener('click', function() {
+  window.location.href = 'https://archivos-de-codigos.onrender.com';
+});
