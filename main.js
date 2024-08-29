b6c3066e7a8784c83e75db23a96c520cdf4acbaa
ArthurBaiza01 <138905503+ArturoBaiza@users.noreply.github.com>
2024-08-28 19:58:28 -0600

    Update  2 main.js

--- main.js
+++ main.js
@@ -1,3 +1,3 @@
 document.getElementById('btn-contacto').addEventListener('click', function() {
-  window.location.href = 'https://mail.google.com/mail/?view=cm&fs=1&to=feriacientifica5tobaco@gmail.com';
+  window.location.href = 'https://mail.google.com/mail/?view=cm&fs=1&to=centro_edu_siloe@hotmail.com';
 });
